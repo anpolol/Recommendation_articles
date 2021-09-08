8a38419df8313fc6f2ca73cbcd8834875205ae94
anpolol <polinaspb@ya.ru>
2021-09-08 17:17:52 +0300

    256 embeddings; remove num of nodes in init

--- Model.py
+++ Model.py
@@ -3,32 +3,39 @@ from torch.nn import ModuleList
 from torch_geometric.nn import SAGEConv
 import torch
 from torch_sparse import SparseTensor
+from torch_geometric.utils import to_undirected
 class Scalable(ScalableGNN):
-    def __init__(self, num_nodes, in_channels, hidden_channels,
-                 out_channels, num_layers,):
+    def __init__(self, in_channels, hidden_channels,
+                 out_channels, num_layers,num_of_nodes=1000):
         # * pool_size determines the number of pinned CPU buffers
         # * buffer_size determines the size of pinned CPU buffers,
         #   i.e. the maximum number of out-of-mini-batch nodes
 
-        super().__init__(num_nodes, hidden_channels, num_layers,
-                         pool_size=2, buffer_size=5000)
+        super().__init__(num_of_nodes,hidden_channels, num_layers,
+                         pool_size=4, buffer_size=70000)
         self.out_channels = out_channels
         self.convs = ModuleList()
         self.convs.append(SAGEConv(in_channels, hidden_channels))
+        
         for _ in range(num_layers - 2):
             self.convs.append(SAGEConv(hidden_channels, hidden_channels))
         self.convs.append(SAGEConv(hidden_channels, out_channels))
 
-    def forward(self, x, adj_t, *args):
-        for i, (conv, history) in enumerate(zip(self.convs, self.histories)):
-            if i == len(self.convs) - 1:
-                x = conv(x, adj_t)
-            else:
-                x = conv(x, adj_t).relu_()
-            x = self.push_and_pull(history, x, *args)
-
-        return x  # self.convs[-1](x, adj_t)
-
+    def forward(self, x, adjs, *args):
+        for i, (edge_index, _, size) in enumerate(adjs):
+            x_target = x[:size[1]]  # Target nodes are always placed first.
+            x = self.convs[i]((x,x_target), edge_index)
+            if i != self.num_layers - 1:
+                x = x.relu_()
+                x = self.push_and_pull(self.histories[i], x, *args)    
+        return x  
+    def forward_for_val(self,x,adjs):
+        for i, (edge_index, _, size) in enumerate(adjs):
+            x_target = x[:size[1]]  # Target nodes are always placed first.
+            x = self.convs[i]((x,x_target), edge_index)
+            if i != self.num_layers - 1:
+                x = x.relu_()
+        return x
     def inference(self, new_vertices,old_vertices, rels, *args):
         # предполагаю что среди neighbors_of_neighbors- нет новых
         # нет проверки на то, пришли ко мне одинаковые вершины или разные
@@ -53,7 +60,7 @@ class Scalable(ScalableGNN):
         # делаем ребра
         batch_edge_index = list(map( lambda x: (map_old_to_new[x[0]],map_old_to_new[x[1]]), rels))
         batch_edge_index = torch.tensor(batch_edge_index).t()
-
+        batch_edge_index=to_undirected(batch_edge_index)
         # собираем х!!!!
         
         x = torch.tensor(embeddings)
@@ -74,9 +81,7 @@ class Scalable(ScalableGNN):
         (pos_rw, neg_rw) = PosNegSamples
         # Negative loss
         start, rest = neg_rw[:, 0].type(torch.LongTensor), neg_rw[:, 1:].type(torch.LongTensor).contiguous()
-        
         h_start = out[start].view(neg_rw.size(0), 1, self.out_channels)
-
         h_rest = out[rest.view(-1)].view(neg_rw.size(0), -1, self.out_channels)
         dot = (h_start * h_rest).sum(dim=-1).view(-1)
         neg_loss = -torch.log(torch.sigmoid((-1) * dot)).mean()
@@ -84,7 +89,6 @@ class Scalable(ScalableGNN):
         if len(pos_rw)!=0:
             start, rest = pos_rw[:, 0].type(torch.LongTensor), pos_rw[:, 1].type(torch.LongTensor).contiguous()
             h_start = out[start].view(pos_rw.size(0), 1, self.out_channels)
-
             h_rest = out[rest.view(-1)].view(pos_rw.size(0), -1, self.out_channels)
             dot = ((h_start * h_rest).sum(dim=-1)).view(-1)
             pos_loss = -(torch.log(torch.sigmoid(dot))).mean()
